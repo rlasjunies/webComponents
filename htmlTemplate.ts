d37ecbd014133faace5db702314d4d6c045e6bc9
Richard Lasjunies <rlasjunies@gmail.com>
2017-04-15 11:09:54 +0200

    htmlTemplate

---
+++ htmlTemplate.ts
@@ -0,0 +1,26 @@
+
+window.onload = (e) => {
+    const movies = [
+        {
+            title: "Harry Potter",
+            description: "Jeune homme a l'école des sorciers"
+        },
+        {
+            title: "Star Trek",
+            description: "Space travellers"
+        },
+        {
+            title: "Avengers",
+            description: "Group of friends"
+        },
+    ];
+
+    const $movieTmpl = document.getElementById("movie");
+    const $movies = document.getElementById("movies");
+    movies.forEach(movie => {
+        const $clone = document.importNode($movieTmpl.content, true);
+        $clone.querySelector("h1").innerText = movie.title;
+        $clone.querySelector("p").innerText = movie.description;
+        $movies.appendChild($clone);
+    });
+};
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 Interesting links:
 
 https://www.html5rocks.com/en/tutorials/webcomponents/customelements/
+https://www.html5rocks.com/en/tutorials/webcomponents/template/
